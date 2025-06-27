c1b12deef2ee60e7418cb1bfdccc6a9d011bc930
= <=>
2025-06-27 15:43:11 +0400

    feat(add): Added ensure installed mason lsp

---
+++ lazy/nvim/lua/plugins/masonconfig.lua
@@ -0,0 +1,15 @@
+return {
+  opts = {
+    ensure_installed = {
+      "stylua",
+      "shfmt",
+      "css-lsp",
+      "html-lsp",
+      "phpactor",
+      "prettier",
+      "tailwindcss-language-server",
+      "tlint",
+      "typescript-language-server",
+    },
+  },
+}
